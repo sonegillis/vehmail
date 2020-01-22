9585fb81168a14f336c24a050c3be604894c5896
G.Mekolle <sone_gillis@yahoo.com>
2020-01-22 20:43:14 +0100

    commit

--- src/app/dashboard-components/mail-list/mail-list.component.ts
+++ src/app/dashboard-components/mail-list/mail-list.component.ts
@@ -1,35 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface Mail {
-  selected: boolean;
-  sender: string;
-  message: string;
-  date: string;
-}
-
-const ELEMENT_DATA: Mail[] = [
-  {selected: false, sender: 'Sone Gillis', message: 'Interview for top US companies next week. Please take short Multiple Quizzes for the invitation', date: 'Jan 15'},
-  {selected: false, sender: 'Sone Gillis', message: 'Interview for top US companies next week. Please take short Multiple Quizzes for the invitation', date: 'Jan 15'},
-  {selected: false, sender: 'Sone Gillis', message: 'Interview for top US companies next week. Please take short Multiple Quizzes for the invitation', date: 'Jan 15'},
-  {selected: false, sender: 'Sone Gillis', message: 'Interview for top US companies next week. Please take short Multiple Quizzes for the invitation', date: 'Jan 15'},
-  {selected: false, sender: 'Sone Gillis', message: 'Interview for top US companies next week. Please take short Multiple Quizzes for the invitation', date: 'Jan 15'},
-  {selected: false, sender: 'Sone Gillis', message: 'Interview for top US companies next week. Please take short Multiple Quizzes for the invitation', date: 'Jan 15'},
-  {selected: false, sender: 'Sone Gillis', message: 'Interview for top US companies next week. Please take short Multiple Quizzes for the invitation', date: 'Jan 15'},
-  {selected: false, sender: 'Sone Gillis', message: 'Interview for top US companies next week. Please take short Multiple Quizzes for the invitation', date: 'Jan 15'}
-];
-
-/**
- * @title Basic use of `<table mat-table>`
- */
-
 @Component({
   selector: 'app-mail-list',
   templateUrl: './mail-list.component.html',
   styleUrls: ['./mail-list.component.scss']
 })
 export class MailListComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
