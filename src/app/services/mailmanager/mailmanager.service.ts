7f1d0995b2a4f881b4a51263df632146805ef3d3
G.Mekolle <sone_gillis@yahoo.com>
2020-01-29 22:18:23 +0100

    commit

--- src/app/services/mailmanager/mailmanager.service.ts
+++ src/app/services/mailmanager/mailmanager.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {Mail} from '../../models/mail';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MailmanagerService {
-  private let _drafts:
+  private _drafts: Mail;
+
+  get drafts(): Mail {
+    return this._drafts;
+  }
+
+  set drafts(value: Mail) {
+    this._drafts = value;
+  }
+
   constructor() { }
 }
